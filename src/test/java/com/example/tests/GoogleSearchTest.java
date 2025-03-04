7b3ffd48e1590fabe7b384e53f9f145948d9585d
Ali Baloch <62090625+balochali@users.noreply.github.com>
2025-03-04 14:09:30 +0500

    Testing Selenium Written First Test Case

---
+++ src/test/java/com/example/tests/GoogleSearchTest.java
@@ -0,0 +1,33 @@
+package com.example.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class GoogleSearchTest {
+    WebDriver driver;
+
+    @BeforeTest
+    public void setup() {
+        // Automatically download and setup ChromeDriver
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void openGoogleHomePage() {
+        driver.get("https://www.google.com");
+        System.out.println("Google Homepage Opened: " + driver.getTitle());
+    }
+
+    @AfterTest
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
